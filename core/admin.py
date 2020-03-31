3e37233fa2070fed28db4fcec5d9a559334395ce
thepsalmist <xf.xavierfrank@gmail.com>
2020-03-31 15:51:22 +0300

    create models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Testimonial, About, Contact, Faq
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ["client_name", "client_title", "message"]
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "message", "publish_date"]
+
+
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ["title", "body"]
--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonial(models.Model):
+    client_name = models.CharField(max_length=100)
+    client_title = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.client_name
+
+
+class About(models.Model):
+    title = models.CharField(default="About us")
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message by {self.name}"
+
+
+class Faq(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
