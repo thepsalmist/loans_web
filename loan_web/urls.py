43a391eb9254cf8aa03ccd35c2453479857bb2d1
thepsalmist <xf.xavierfrank@gmail.com>
2020-04-15 09:42:31 +0300

    modify base settings

--- loan_web/urls.py
+++ loan_web/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -26,4 +27,4 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
-
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
