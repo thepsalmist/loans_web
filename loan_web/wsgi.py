8dc29b5f41333f64f4ff679a128782212a095d46
thepsalmist <xf.xavierfrank@gmail.com>
2020-04-15 09:33:52 +0300

    modify production variables

--- loan_web/wsgi.py
+++ loan_web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loan_web.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "loan_web.settings.production")
 
 application = get_wsgi_application()
