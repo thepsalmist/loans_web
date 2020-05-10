3c3ee2beb735d7e744eb6e76096bd7e79e148d12
thepsalmist <xf.xavierfrank@gmail.com>
2020-05-10 16:11:42 +0300

    modify ads.txt file

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 
 app_name = "core"
@@ -6,4 +8,7 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="home"),
     path("privacy/", views.privacy, name="privacy"),
+    path(
+        "app-ads.txt", RedirectView.as_view(url=staticfiles_storage.url("app-ads.txt")),
+    ),
 ]
